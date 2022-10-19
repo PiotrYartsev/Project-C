35f205872d03eb89847706a09ade8f5a2216758d
Piotr Yartsev <piotr_1997@live.se>
2022-10-19 13:53:36 +0200

    fler saker

--- random-walker-2D-rock-papper-sizors.py
+++ random-walker-2D-rock-papper-sizors.py
@@ -34,7 +34,7 @@ while p < number_of_balls:
 
 where_points_rock=[]
 where_points_paper=[]
-where_points_sizors=[]
+where_points_scissors=[]
 
 for i in range(len(x1)):
     for j in range(len(x1[i])):
@@ -45,15 +45,15 @@ for i in range(len(x1)):
             elif randomvar < 2/3 and randomvar > 1/3:
                 where_points_paper.append([i,j])
             else:
-                where_points_sizors.append([i,j])
+                where_points_scissors.append([i,j])
             
             
 print("rock")
 print(where_points_rock)
 print("paper")
 print(where_points_paper)
-print("sizors")
-print(where_points_sizors)
+print("scissors")
+print(where_points_scissors)
 
 
 resulting_points2=[]
@@ -124,14 +124,14 @@ def moving_and_shecking(new_position,you,beatyou,youbeat,direction,size):
 
 newrock=[]
 newpaper=[]
-newsizors=[]
-def run_this_code(where_points_rock,where_points_paper,where_points_sizors,all_points,limit):
+newscissors=[]
+def run_this_code(where_points_rock,where_points_paper,where_points_scissors,all_points,limit):
 
     #shoose to move left or right
     number_of_steps=0
-    while number_of_steps < limit and len(where_points_rock)+len(where_points_paper)+len(where_points_sizors) > 0:
+    while len(where_points_rock) > 0 and len(where_points_paper) > 0 or len(where_points_scissors) > 0 and len(where_points_paper) > 0 or len(where_points_rock) > 0 and len(where_points_scissors) > 0 or len(where_points_scissors) > 0 and len(where_points_paper) > 0:
         size=len(all_points)
-        total=len(where_points_rock)+len(where_points_paper)+len(where_points_sizors)
+        total=len(where_points_rock)+len(where_points_paper)+len(where_points_scissors)
         randomvar=np.random.random()
 
         #rock
@@ -140,7 +140,7 @@ def run_this_code(where_points_rock,where_points_paper,where_points_sizors,all_p
             
             point,direction=choose_point_and_direction(where_points_rock)
             newpoint=[point[0]+direction[0],point[1]+direction[1]]
-            result =(moving_and_shecking(newpoint,where_points_rock,where_points_paper,where_points_sizors,direction,size))
+            result =(moving_and_shecking(newpoint,where_points_rock,where_points_paper,where_points_scissors,direction,size))
             if result=="delete":
                 where_points_rock.remove(point)
             elif result=="dont move":
@@ -154,9 +154,9 @@ def run_this_code(where_points_rock,where_points_paper,where_points_sizors,all_p
             elif result=="win":
                 try:
                     where_points_rock.append(newpoint)
-                    where_points_sizors.remove(newpoint)
+                    where_points_scissors.remove(newpoint)
                 except:
-                    raise Exception("win",newpoint,where_points_sizors)
+                    raise Exception("win",newpoint,where_points_scissors)
             elif result=="move":
                 try:
                     where_points_rock.remove(point)
@@ -180,7 +180,7 @@ def run_this_code(where_points_rock,where_points_paper,where_points_sizors,all_p
             point,direction=choose_point_and_direction(where_points_paper)
             #print(point)
             newpoint=[point[0]+direction[0],point[1]+direction[1]]
-            result =(moving_and_shecking(newpoint,where_points_paper,where_points_sizors,where_points_rock,direction,size))
+            result =(moving_and_shecking(newpoint,where_points_paper,where_points_scissors,where_points_rock,direction,size))
             if result=="delete":
                 where_points_paper.remove(point)
             elif result=="dont move":
@@ -188,9 +188,9 @@ def run_this_code(where_points_rock,where_points_paper,where_points_sizors,all_p
             elif result=="die":
                 try:
                     where_points_paper.remove(point)
-                    where_points_sizors.append(newpoint)
+                    where_points_scissors.append(newpoint)
                 except:
-                    raise Exception("die",newpoint,where_points_sizors)
+                    raise Exception("die",newpoint,where_points_scissors)
             elif result=="win":
                 try:
                     where_points_paper.append(newpoint)
@@ -211,21 +211,21 @@ def run_this_code(where_points_rock,where_points_paper,where_points_sizors,all_p
 
 
 
-        #sizors
+        #scissors
         else:
-            #print("sizors")
+            #print("scissors")
 
-            point,direction=choose_point_and_direction(where_points_sizors)
+            point,direction=choose_point_and_direction(where_points_scissors)
             #print(point)
             newpoint=[point[0]+direction[0],point[1]+direction[1]]
-            result=(moving_and_shecking(newpoint,where_points_sizors,where_points_rock,where_points_paper,direction,size))
+            result=(moving_and_shecking(newpoint,where_points_scissors,where_points_rock,where_points_paper,direction,size))
             if result=="delete":
-                where_points_sizors.remove(point)
+                where_points_scissors.remove(point)
             elif result=="dont move":
                 pass
             elif result=="die":
                 try:
-                    where_points_sizors.remove(point)
+                    where_points_scissors.remove(point)
                     where_points_rock.append(newpoint)
                 except:
                     raise Exception("die",newpoint,where_points_rock)
@@ -233,30 +233,30 @@ def run_this_code(where_points_rock,where_points_paper,where_points_sizors,all_p
             elif result=="win":
                 try:
 
-                    where_points_sizors.append(newpoint)
+                    where_points_scissors.append(newpoint)
                     where_points_paper.remove(newpoint)
                 except:
                     raise Exception("win",newpoint,where_points_paper)
 
             elif result=="move":
                 try:
-                    where_points_sizors.remove(point)
-                    where_points_sizors.append(newpoint)
+                    where_points_scissors.remove(point)
+                    where_points_scissors.append(newpoint)
                 except:
-                    raise Exception("move",newpoint,where_points_sizors)
+                    raise Exception("move",newpoint,where_points_scissors)
         
         number_of_steps+=1
 
         
         newrock.append(str(where_points_rock[:]))
         newpaper.append(str(where_points_paper[:]))
-        newsizors.append(str(where_points_sizors[:]))
+        newscissors.append(str(where_points_scissors[:]))
     return(number_of_steps)
 
 
 """
 
-run_this_code(where_points_rock,where_points_paper,where_points_sizors,x1)
+run_this_code(where_points_rock,where_points_paper,where_points_scissors,x1)
 """
 def str_to_list(resulting_points):
     second_resulting_points=[]
@@ -281,7 +281,7 @@ def str_to_list(resulting_points):
 """
 newrock=str_to_list(newrock)
 newpaper=str_to_list(newpaper)
-newsizors=str_to_list(newsizors)
+newscissors=str_to_list(newscissors)
 
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
@@ -289,7 +289,7 @@ import numpy as np
 
 fig, ax = plt.subplots()
 
-ax.set_title('Rock/Paper/Sizors: 2D')
+ax.set_title('Rock/Paper/scissors: 2D')
 ax.set_xlabel('Position in x')
 ax.set_ylabel('Position in y')
 #Set size of plot
@@ -308,7 +308,7 @@ plt.plot([size+0.5,-0.5],[-0.5,-0.5],color='r', linestyle='-', linewidth=1)
 # set up empty lines to be updates later on
 rock, = ax.plot([],[],'b*',label="Rock")
 paper, = ax.plot([],[],'r+',label="Paper")
-sizors, = ax.plot([],[],'go',label="Sizors")
+scissors, = ax.plot([],[],'go',label="scissors")
 
 def gen1():
     i = 0
@@ -330,9 +330,9 @@ def run1(c):
     y_p=[o[1] for o in newpaper[c]]
     paper.set_data(x_p,y_p)
 
-    x_s=[o[0] for o in newsizors[c]]
-    y_s=[o[1] for o in newsizors[c]]
-    sizors.set_data(x_s,y_s)
+    x_s=[o[0] for o in newscissors[c]]
+    y_s=[o[1] for o in newscissors[c]]
+    scissors.set_data(x_s,y_s)
 
 
     #l2.set_data(what_happened[c][0],what_happened[c][1])
@@ -344,17 +344,17 @@ ani1 = animation.FuncAnimation(fig,run1,gen1,interval=400,blit=False,repeat=Fals
 plt.show()
 #save the ani1 and ani2 as a gif
 
-ani1.save('plots/rock-paper_sizors.gif', fps=3,writer='imagemagick')
+ani1.save('plots/rock-paper_scissors.gif', fps=3,writer='imagemagick')
 """
 
 plot_number_of_steps_rock=[]
 plot_number_of_steps_paper=[]
-plot_number_of_steps_sizors=[]
+plot_number_of_steps_scissors=[]
 numberofsteps=10000
 for n in tqdm(range(numberofsteps)):
     #make an array of 1000 zeros
     size=10
-    number_of_balls=40
+    number_of_balls=10
     #make 2d array
     x1 = np.zeros((size,size))
 
@@ -369,7 +369,7 @@ for n in tqdm(range(numberofsteps)):
 
     where_points_rock=[]
     where_points_paper=[]
-    where_points_sizors=[]
+    where_points_scissors=[]
 
     for i in range(len(x1)):
         for j in range(len(x1[i])):
@@ -380,41 +380,31 @@ for n in tqdm(range(numberofsteps)):
                 elif randomvar < 2/3 and randomvar > 1/3:
                     where_points_paper.append([i,j])
                 else:
-                    where_points_sizors.append([i,j])
+                    where_points_scissors.append([i,j])
     newrock=[]
     newpaper=[]
-    newsizors=[]     
+    newscissors=[]     
     newrock=str_to_list(newrock)
     newpaper=str_to_list(newpaper)
-    newsizors=str_to_list(newsizors)
+    newscissors=str_to_list(newscissors)
 
 
     limit=400
-    number_of_steps=run_this_code(where_points_rock,where_points_paper,where_points_sizors,x1,limit)
+    number_of_steps=run_this_code(where_points_rock,where_points_paper,where_points_scissors,x1,limit)
+    plot_number_of_steps_rock.append(number_of_steps)
     
-    plot_number_of_steps_rock.append(len(newrock[-1]))
-    plot_number_of_steps_paper.append(len(newpaper[-1]))
-    plot_number_of_steps_sizors.append(len(newsizors[-1]))
+
     
 
 
 numberofbins=int(len(list(set(plot_number_of_steps_rock)))/2)
-fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15,5))
-axes[0].hist(plot_number_of_steps_rock,color="r", bins=numberofbins,label="Number of\nrocks left".format(number_of_balls))
-axes[1].hist(plot_number_of_steps_paper,color="b", bins=numberofbins,label="Number of\npapers left".format(number_of_balls))
-axes[2].hist(plot_number_of_steps_sizors,color="g", bins=numberofbins,label="Number of\nsizors left".format(number_of_balls))
-fig.tight_layout()
-
-
-axes[0].legend()
-axes[1].legend()
-axes[2].legend()
-
-fig.supxlabel('Number of steps')
-fig.supylabel('Number of times')
-fig.suptitle('Number of items after {} iterations'.format(limit))
+plt.hist(plot_number_of_steps_rock,bins=numberofbins,color="blue",alpha=0.5,label="Starting with {} balls".format(number_of_balls))
+plt.legend(loc="upper right")
+plt.xlabel('Number of steps')
+plt.ylabel('Number of times')
+plt.title('Number of steps to get to only one surviving group\nin a {}x{} grid starting at {} balls'.format(size,size,number_of_balls))
 #plt.show()
 plt.tight_layout()
-plt.savefig('plots/rock-paper-sizors-after-{}.png'.format(limit))
+plt.savefig('plots/rock-paper-scissors-{}-{}.png'.format(size,number_of_balls))
 
 #"""
